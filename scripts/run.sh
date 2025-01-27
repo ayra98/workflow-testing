6340c8ec7c5dbcfdb43284dcd20ac6b8f1a34f2e
ayra98 <aryamohan98@gmail.com>
2025-01-27 09:08:39 +0530

    Update file

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,23 +6,12 @@ credentials=$REGITTOKEN
 upload_to_playstore=$UPLOAD_TO_PLAYSTORE
 repetativeBuildCheck=$REPETATIVE_BUILD_CHECK
 hotfix=$HOTFIX
-echo "$BRANCH"
-
-# for buildBusiness in "${buildBusinessList[@]}";
-# do
-#   echo "Build Business is :- " $buildBusiness
-#   for buildType in "${buildTypes[@]}";
-#   do
-#     echo "Build Type is :- " + $buildType
-#     for buildEnv in "${buildEnvs[@]}";
-#     do
-#       echo "Build Env is :- " + $buildEnv
-#       echo $buildType | awk '{gsub(/^ +| +$/,"")}1'
-#       curl --location --request POST 'https://api.github.com/repos/hotstar/hotstar-android-mobile/actions/workflows/29366532/dispatches' \
-#         --header 'Accept: application/vnd.github.v3+json' \
-#         --header "Authorization: $credentials" \
-#         --header 'Content-Type: application/json' \
-#         --data-raw '{"ref": "'"$BRANCH"'", "inputs": {"BUILD_ENV": "'$buildEnv'", "BUILD_TYPE": "'$buildType'", "BUILD_BUSINESS": "'$buildBusiness'" , "UPLOAD_TO_PLAYSTORE": "'$upload_to_playstore'" , "REPETATIVE_BUILD_CHECK": "'$repetativeBuildCheck'" , "HOTFIX": "'$hotfix'"}}'
-#     done
-#   done
-# done
+echo "=== Starting Build Script ==="
+echo "Branch: $BRANCH"
+echo "Build Types: ${buildTypes[@]}"
+echo "Build Envs: ${buildEnvs[@]}"
+echo "Build Business List: ${buildBusinessList[@]}"
+echo "Upload to Playstore: $upload_to_playstore"
+echo "Repetitive Build Check: $repetativeBuildCheck"
+echo "Hotfix: $hotfix"
+echo "=============================="
