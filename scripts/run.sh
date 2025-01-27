81ec7e7b7ad8d7280c936cf46fa221a36a58bbb2
ayra98 <aryamohan98@gmail.com>
2025-01-27 08:56:35 +0530

    initial commit

---
+++ scripts/run.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+buildTypes=($BUILD_TYPE)
+buildEnvs=($BUILD_ENV)
+buildBusinessList=($BUILD_BUSINESS)
+credentials=$REGITTOKEN
+upload_to_playstore=$UPLOAD_TO_PLAYSTORE
+repetativeBuildCheck=$REPETATIVE_BUILD_CHECK
+hotfix=$HOTFIX
+echo "$BRANCH"
+
+# for buildBusiness in "${buildBusinessList[@]}";
+# do
+#   echo "Build Business is :- " $buildBusiness
+#   for buildType in "${buildTypes[@]}";
+#   do
+#     echo "Build Type is :- " + $buildType
+#     for buildEnv in "${buildEnvs[@]}";
+#     do
+#       echo "Build Env is :- " + $buildEnv
+#       echo $buildType | awk '{gsub(/^ +| +$/,"")}1'
+#       curl --location --request POST 'https://api.github.com/repos/hotstar/hotstar-android-mobile/actions/workflows/29366532/dispatches' \
+#         --header 'Accept: application/vnd.github.v3+json' \
+#         --header "Authorization: $credentials" \
+#         --header 'Content-Type: application/json' \
+#         --data-raw '{"ref": "'"$BRANCH"'", "inputs": {"BUILD_ENV": "'$buildEnv'", "BUILD_TYPE": "'$buildType'", "BUILD_BUSINESS": "'$buildBusiness'" , "UPLOAD_TO_PLAYSTORE": "'$upload_to_playstore'" , "REPETATIVE_BUILD_CHECK": "'$repetativeBuildCheck'" , "HOTFIX": "'$hotfix'"}}'
+#     done
+#   done
+# done
