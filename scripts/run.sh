40350b55571ffa9d14f05e95c0bec5583525d09a
ayra98 <103246894+ayra98@users.noreply.github.com>
2025-02-18 12:09:52 +0530

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -15,3 +15,34 @@ echo "Upload to Playstore: $upload_to_playstore"
 echo "Repetitive Build Check: $repetativeBuildCheck"
 echo "Hotfix: $hotfix"
 echo "=============================="
+
+
+for buildBusiness in "${buildBusinessList[@]}"; do
+  echo "Build Business is: $buildBusiness"
+  for buildType in "${buildTypes[@]}"; do
+    echo "Build Type is: $buildType"
+    for buildEnv in "${buildEnvs[@]}"; do
+      echo "Build Env is: $buildEnv"
+      
+      # Optionally trim whitespace from buildType if needed
+      trimmedBuildType=$(echo "$buildType" | awk '{gsub(/^ +| +$/,"")}1')
+      
+      # Dispatch event to the new workflow (ci.yml) in the ayra98/workflow-testing repository
+      curl --location --request POST 'https://api.github.com/repos/ayra98/workflow-testing/actions/workflows/ci.yml/dispatches' \
+        --header 'Accept: application/vnd.github.v3+json' \
+        --header "Authorization: $credentials" \
+        --header 'Content-Type: application/json' \
+        --data-raw '{
+          "ref": "'"$BRANCH"'",
+          "inputs": {
+            "BUILD_ENV": "'"$buildEnv"'",
+            "BUILD_TYPE": "'"$trimmedBuildType"'",
+            "BUILD_BUSINESS": "'"$buildBusiness"'",
+            "UPLOAD_TO_PLAYSTORE": "'"$upload_to_playstore"'",
+            "REPETATIVE_BUILD_CHECK": "'"$repetativeBuildCheck"'",
+            "HOTFIX": "'"$hotfix"'"
+          }
+        }'
+    done
+  done
+done
